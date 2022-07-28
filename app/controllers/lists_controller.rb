d94bc6d1c26ecc50f3c70ec0ae4afc6bfbf6c244
tommytommy-ヒーロー <109708488+tommytommy-hero@users.noreply.github.com>
2022-07-28 23:56:45 +0000

    practice

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,48 @@
+class ListsController < ApplicationController
+  def new
+   @list = List.new
+  end
+  
+  def create
+    #1/&2.データを受け取り新規登録するためのインスタンス作成
+   @list = List.new(list_params)
+    #３。データをデータベースに保存するためのsaveメソッド実行
+   if @list.save
+    #4.トップ画面へリダイレクト
+    redirect_to list_path(@list.id)
+   else
+    render :new
+   end
+  end
+
+  def index
+   @lists = List.all
+  end
+
+  def show
+   @list = List.find(params[:id])
+  end
+
+  def edit
+   @list = List.find(params[:id])
+  end
+  
+  def update
+   list = List.find(params[:id])
+   list.update(list_params)
+   redirect_to list_path(list.id)
+  end
+  
+  def destroy
+   list = List.find(params[:id]) 
+   list.destroy
+   redirect_to '/lists'         
+  end
+  
+  private
+  #ストロングパラメータ
+  def list_params
+   params.require(:list).permit(:title,:body,:image)
+  end
+  
+end
